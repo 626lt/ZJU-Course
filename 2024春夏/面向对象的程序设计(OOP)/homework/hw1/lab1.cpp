48dbcbf49629b475bc8593ba4dc5b0595b3cda7a
Tao liu <129494413+626lt@users.noreply.github.com>
2024-03-12 14:52:18 +0800

    完成OOP HW1；人工智能逻辑作业2

--- 2024春夏/面向对象的程序设计(OOP)/homework/hw1/lab1.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<fstream>
-#include<string>
-using namespace std;
-int main() 
-{
-    cout << "no      name    score1  score2  score3  average" << endl;
-    return 0;
-}
