75c2b1cbbf9685b91e66b256acd4960469482951
Tao liu <129494413+626lt@users.noreply.github.com>
2024-03-21 15:50:26 +0800

    Update divider.v

--- 2024春夏/计算机组成与设计/lab3/divider.v
+++ 2024春夏/计算机组成与设计/lab3/divider.v
@@ -9,47 +9,52 @@ module divider(
     output[31:0] res,       // 商
     output[31:0] rem        // 余数
 );
-    reg [63:0] high_divisor;
-    reg [63:0] remainder;
-    reg [4:0] cnt;
-    reg [31:0] Quotient;
-    reg state;//state = 0 notrunning, state = 1 running
 
-    assign remainder = {32'b0, dividend};
-    assign high_divisor = {divisor, 32'b0};
-    // assign res = Quotient;
-    // assign rem = remainder[31:0];
-    
+    reg [63:0] remainder;
+    reg [31:0] quotient;
+    reg [63:0] high_divisor;
+    reg [5:0] count;
+    reg state;
 
     always @(posedge clk or posedge rst) begin
-        if (rst) begin
+        if(rst) begin
             divide_zero <= 0;
             finish <= 0;
+            res <= 0;
+            rem <= 0;
             state <= 0;
+            count <= 0;
+            remainder <= 0;
+            quotient <= 0;
+            high_divisor <= 0;
         end
-        else if(start) begin // Not Running
-            state = 1;
-            if (divisor == 0) begin
+        else if(start && ~state) begin
+            state <= 1;
+            if(divisor == 0) begin
                 finish = 1;
                 divide_zero = 1;
             end
-            else if(state & ~finish) begin
-                if(high_divisor < remainder) begin
-                    remainder = remainder - high_divisor;
-                    Quotient[0] = 1;   
-                end
-                Quotient = Quotient << 1;
-                high_divisor = high_divisor >> 1;
-                cnt = cnt + 1;
+            else begin
+                remainder = {32'b0, dividend};
+                high_divisor = {divisor, 32'b0};
+                count = 0;
+                quotient = 0;
+                finish = 0;
             end
         end
-        if(cnt == 0 && state) begin
-            finish = 1;
+        else if(state && ~finish) begin
+            if(remainder > high_divisor) begin
+                remainder = remainder - high_divisor;
+                quotient[0] = 1;
+            end
+            quotient = quotient << 1;
+            high_divisor >> 1;
+            count = count + 1;
+        end
+
+        if(count == 32 && state) begin
             state = 0;
-            assign res = Quotient;
-            assign rem = remainder[31:0];
+            finish = 1;
         end
     end
-
-
 endmodule
